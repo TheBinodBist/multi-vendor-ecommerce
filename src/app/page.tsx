ec3eb79dd44c83404f2acf6bd7db6a6b9ed512dc
Binod Bist <thebinodbist@gmail.com>
2025-11-21 21:22:56 +0545

    components custimized

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>hello there</h1>
+    <div className="text-red-400">
+      <Button variant={"elevated"}>hello</Button>
+      <Input placeholder="i am input" />
+    </div>
+
   );
 }
