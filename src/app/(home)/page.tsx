c0ebea9449ba0f4668e7711da902d6d0f8d1dc27
Binod Bist <thebinodbist@gmail.com>
2025-11-21 22:37:06 +0545

    layput added

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 import Image from "next/image";
 
 export default function Home() {
@@ -7,6 +8,9 @@ export default function Home() {
     <div className="text-red-400">
       <Button variant={"elevated"}>hello</Button>
       <Input placeholder="i am input" />
+      <Textarea placeholder="i am textarea">
+        
+      </Textarea>
     </div>
 
   );
