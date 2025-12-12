b329c46fcd67d930583aec9475d029db0e3d86f2
Binod Bist <thebinodbist@gmail.com>
2025-12-12 20:37:55 +0545

    final search filter

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,11 +1,15 @@
+import { getQueryClient , trpc} from '@/trpc/server'
+import { stringify } from 'querystring';
 import React from 'react'
 
-const page = () => {
+const Home = async () => {
+  const queryClient = getQueryClient();
+  const categories = await queryClient.fetchQuery(trpc.categories.getMany.queryOptions())
   return (
     <div>
-      home
+      {JSON.stringify(categories,null,2)}
     </div>
   )
 }
 
-export default page
+export default Home
--- src/app/(app)/(home)/search-filters/index.tsx
+++ src/app/(app)/(home)/search-filters/index.tsx
@@ -3,16 +3,18 @@ import SearchInput from './search-input'
 import Categories from './categories'
 import { CustomCategory } from '../types'
 
+// Ensure data is typed as an array
 interface Props{
-  data: CustomCategory;
+  data: CustomCategory[]; 
 }
 
 const SearchFilters = ({data}:Props) => {
   return (
     <div className='px-4 lg:px-12 py-8 border-b flex-col gap-4 w-full'>
-        <SearchInput />
+        <SearchInput data={data} disabled={false} />
+        
         <div className='hidden lg:block'>
-        <Categories data={data}/>
+            <Categories data={data}/>
         </div>
     </div>
   )
