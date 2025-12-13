e2efbaf303d619b4ae820e95117da9337b72945c
Binod Bist <thebinodbist@gmail.com>
2025-12-13 13:29:02 +0545

    ...,

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -1,52 +1,26 @@
-
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query'
 import React from 'react'
 import Navbar from './navbar'
 import Footer from './footer'
 
-import configPromise from '@payload-config'
-import { getPayload } from 'payload'
-import {Category} from '@/payload-types'
 import SearchFilters from './search-filters'
-import { CustomCategory } from './types'
+import { getQueryClient, trpc } from '@/trpc/server'
 
 interface Props {
   children: React.ReactNode
 }
 
 export default async function Layout({ children }: Props) {
-  const payload = await getPayload({
-    config: configPromise,
-  })
-
-  const data = await payload.find({
-    collection: "categories",
-    depth: 1,  // populate subcategories
-    pagination: false,
-    where: {
-      parent: {
-        exists: false
-      }
-    },
-    sort:"name"
-  })
-
-  const formatedData : CustomCategory[]= data.docs.map((doc) => ({
-    ...doc,
-    subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
-      //because of depth 1 we are conindent "doc " will be type of category
-      ...(doc as Category),
-      subcategories:undefined,
-    }))
-  }))
-
-console.log(data,formatedData)
-
-
-
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(
+    trpc.categories.getMany.queryOptions(),
+  )
   return (
     <div className='flex flex-col min-h-screen'>
       <Navbar />
-      <SearchFilters data={formatedData}/>
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <SearchFilters  />
+      </HydrationBoundary>
       <div className='flex-1 bg-[#F4F4f0]'>
         {children}
       </div>
